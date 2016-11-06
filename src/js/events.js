2805a5c77fea2abef76035883f67016964c69f61
Juan Ferreras <juan@flioh.com>
2016-11-06 11:38:25 -0300

    added mock individual event page with carousel functionality

--- src/js/events.js
+++ src/js/events.js
@@ -1,3 +1,107 @@
+function getCurrentItem(){
+  var el = document.querySelector('[role=carousel]')
+  var n = 0
+  for (var i = 0; i<=el.children.length; i++){
+    var item = el.children[i]
+    if (item.classList.contains("active")) {
+      n = i
+      break
+    }
+  }
+  return n
+}
+
+function isCarouselAnimating(){
+  return document.querySelector('[role=carousel]').classList.contains('animating')
+}
+
+function nextItem() {
+  var isAnimating = isCarouselAnimating()
+  if (!isAnimating){
+    var n = getCurrentItem()
+    hideItem('to-left');
+    showItem('from-right');
+  }
+}
+
+function previousItem() {
+  var isAnimating = isCarouselAnimating()
+  if (!isAnimating){
+    var n = getCurrentItem()
+    hideItem('to-right');
+    showItem('from-left');
+  }
+}
+
+function hideItem(direction) {
+  var el = document.querySelector('[role=carousel]')
+  var items = el.children
+  var dots = document.querySelector('[role=carousel-dots]').children
+  var currentItem = getCurrentItem()
+
+  el.classList.add('animating')
+  items[currentItem].classList.add(direction)
+  dots[currentItem].classList.remove('active')
+  items[currentItem].addEventListener('animationend', function() {
+    this.classList.remove('active', direction)
+  });
+}
+
+function showItem(direction) {
+  var el = document.querySelector('[role=carousel]')
+  var items = el.children
+  var dots = document.querySelector('[role=carousel-dots]').children
+  var currentItem = getCurrentItem()
+  
+  currentItem = direction == 'from-left' ? currentItem-1 : currentItem+1
+  currentItem = (currentItem + items.length) % items.length
+
+  items[currentItem].classList.add('next', direction)
+  dots[currentItem].classList.add('active')
+  items[currentItem].addEventListener('animationend', function() {
+    this.classList.remove('next', direction)
+    this.classList.add('active')
+    el.classList.remove('animating')
+  });
+}
+
+function transformEvent (obj){
+  obj = obj || {}
+  obj.videoId = 'WuqpgpHH1r4'
+  obj.description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam interdum nibh massa, quis sollicitudin metus dignissim sed. Suspendisse sollicitudin aliquam orci, egestas feugiat nisl rhoncus rhoncus. Etiam luctus arcu nisl, sed mollis nunc sagittis sit amet. Morbi ac mi venenatis, pulvinar sapien sed, condimentum nibh. Aenean vel ultricies sapien, nec tempor elit. Maecenas iaculis urna non aliquam tincidunt. Fusce lacinia imperdiet lorem eu vulputate. Donec nec aliquet libero, sed auctor est. Praesent ultrices metus ut nunc commodo, sit amet rutrum enim gravida. Nam interdum tincidunt mi id ullamcorper. Mauris rutrum mi commodo, consequat lacus eget, consectetur magna.'
+
+  obj.carousel = []
+  obj.gallery = []
+
+  for (var i = 1; i<31; i++){
+    var placeholder = '/img/event/DD'+i+'.jpg'
+    if (i<4) obj.carousel.push(placeholder)
+    else obj.gallery.push(placeholder)
+  }
+
+  obj.carousel = obj.carousel.map(function(value, currentIndex){
+    preLoadImage(value)
+    return {
+      'index': currentIndex,
+      'isFirst': currentIndex==0,
+      'image': value
+    }
+  })
+  obj.gallery = obj.gallery.map(function(value){
+    return {
+      'src': value.replace('event','event/thumb'),
+      'bigSrc': value
+    }
+  })
+  return obj
+}
+
+function openGalleryModal (e, el) {
+  openModal('gallery-modal', {
+    src: el.getAttribute('big-src')
+  })
+}
+
 function transformEvents (obj) {
   mockAPIEvents(obj)
   obj.events.forEach(function (i, index, arr) {
@@ -5,6 +109,7 @@ function transformEvents (obj) {
     i.upcoming = new Date(i.date) > today
     i.dateString = formatDate(i.date)
     i.date = formatDateJSON(i.date)
+    i.id = "123"
   })
   return obj  
 }
@@ -67,7 +172,7 @@ function mockAPIEvents(obj){
     'title' : "Karma Fields Live | The Hex",
     'location' : "LOT 613",
     'city': "Los Angeles. USA", 
-    'date' : "2016-11-04T20:30:00",
+    'date' : "2016-11-15T20:30:00",
     'monstercat': false
   })
 
