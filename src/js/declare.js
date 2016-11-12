e0ec08c3d8574d79a6fd0db325de06098f0c8ca6
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2016-11-12 10:58:28 -0500

    Use <form/> to enable browsers to save account password.
    Fixes #59.
    
    Browsers do not offer to save login credentials when there is no
    submit event. This change causes my browser to offer to save and
    autocomplete my Monstercat account credentials.

--- src/js/declare.js
+++ src/js/declare.js
@@ -178,6 +178,16 @@ function interceptKeyPress (e) {
   }
 }
 
+function interceptSubmit (e) {
+  if (!e.path) {
+    addEventPath(e)
+  }
+  if (e.path[0].dataset.submitAction) {
+    e.preventDefault()
+    return runSubmitAction(e, e.path[0])
+  }
+}
+
 function interceptClick (e) {
   var isAnchor = false
   var isAction = null
@@ -249,6 +259,11 @@ function getMethod(el, attr) {
 
 function runAction (e, el) {
   var fn = getMethod(el, 'action')
+  if (fn) fn(e, el)
+}
+
+function runSubmitAction (e, el) {
+  var fn = getMethod(el, 'data-submit-action')
   if (fn) fn(e, el)
 }
 
